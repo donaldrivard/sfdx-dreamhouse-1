0438c257da619a5273685c2599f8202d56b12077
Thomas <t.dvornik@salesforce.com>
2016-09-14 21:16:57 -0600

    Put selenium artifacts in output dir. Add checks for IQ Picture Loader in selenium tests.

--- test/integration/IntegrationTest.java
+++ test/integration/IntegrationTest.java
@@ -1,5 +1,6 @@
 package test.integration;
 
+import java.util.List;
 import org.junit.*;
 import org.openqa.selenium.*;
 
@@ -9,14 +10,16 @@ import org.openqa.selenium.*;
 public class IntegrationTest extends BaseSalesforceTest {
     @Test
     public void testIntegration() {
-        final String pageSrc = this.login("/one/one.app");
+        final String pageSrc = this.login("/one/one.app#/sObject/Property__c/home");
+        // Salesforce retUrl will strip the hash. Selenium driver.get() will hang on a hash.
+        // SO set the hash manually.
+        ((JavascriptExecutor) driver).executeScript("window.location.hash='#/sObject/Property__c/home'");
+        this.fluentWait(By.className("salesforceIdentityAppLauncherHeader")).isDisplayed();
 
-        // TODO Check an actual element of the dreamhouse app. We need to fist
-        // click to go to the dreamhouse app, then click property explorer, then
-        // we can check an interesting div, like //.cPropertyExplorer h1"))
-        // OR maybe after data is loaded we can go directly to #/sObject/Property__c/home
+        this.fluentWait(By.linkText("Properties")).click();
+        this.fluentWait(By.linkText("Contemporary Luxury")).click();
 
-        WebElement e = this.fluentWait(new By.ByCssSelector(".oneContent h2"));
-        Assert.assertTrue("One.app was not loaded", e.isDisplayed());
+        WebElement e = this.fluentWait(By.xpath("//span[contains(text(), 'IQ Picture Uploader')]"));
+        Assert.assertTrue("IQ Compontent was not loaded", e.isDisplayed());
     }
 }
