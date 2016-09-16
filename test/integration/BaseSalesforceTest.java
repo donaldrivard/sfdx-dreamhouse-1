fe4bbee82dcf54d5635c9369a05ee1ea1caa1808
Wade Wegner <wade.wegner@gmail.com>
2016-09-16 11:46:01 -0700

    Updated timeouts

--- test/integration/BaseSalesforceTest.java
+++ test/integration/BaseSalesforceTest.java
@@ -61,8 +61,8 @@ public class BaseSalesforceTest {
 
     public WebElement fluentWait(final By locator) {
         Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                .withTimeout(30, TimeUnit.SECONDS)
-                .pollingEvery(5, TimeUnit.SECONDS)
+                .withTimeout(5, TimeUnit.SECONDS)
+                .pollingEvery(1, TimeUnit.SECONDS)
                 .ignoring(NoSuchElementException.class);
 
         WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
